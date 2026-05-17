5d802d1c5611998eb566d25e10f4c8bc365ce9ec
Dharani M <186461332+its-dharani@users.noreply.github.com>
2026-05-17 11:52:40 +0530

    Update SecondLargest.java

--- SecondLargest.java
+++ SecondLargest.java
@@ -2,26 +2,20 @@ public class SecondLargest {
     public static void main(String[] args){
         int[] a = {1,5,7,8,2};
         int n= a.length;
-        int l;
-        int sl;
-        if (a[0] > a[1]) {
-            l = a[0];
-            sl = a[1];
-        } else {
-            l = a[1];
-            sl = a[0];
-        }
-        for(int i=1;i<5;i++){
-            if(a[i]>l && a[i]>sl){
-                sl=l;
+        int l=a[0];
+        for(int i=1;i<n;i++){
+            if (a[i]>l){
                 l=a[i];
-
-            }
-            else if(a[i]<l && a[i]>sl){
-                sl=a[i];
             }
         }
         System.out.println("Largest:"+l);
+
+        int sl=a[0];
+        for(int j=1;j<n;j++){
+            if(a[j]>sl && a[j]!=l){
+                sl=a[j];
+            }
+        }
         System.out.println("Second Largest:"+sl);
 
     }
