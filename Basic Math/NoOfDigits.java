a74cc62d049da379b8e9a19df7f0ccb6ca1d5f6a
Dharani M <186461332+its-dharani@users.noreply.github.com>
2026-06-25 22:22:22 +0530

    Create NoOfDigits.java

---
+++ Basic Math/NoOfDigits.java
@@ -0,0 +1,11 @@
+public class NoOfDigits {
+    public static void main(String[] args){
+        int n=123456;
+        int count=0;
+        while(n>0){
+            n=n/10;
+            count++;
+        }
+        System.out.println("Number of Digits="+count);
+    }
+}
